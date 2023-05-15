fc4d6b0b7e9d1e5eaa4752f21a78570e2fa104b9
ramesh <ramesh@DESKTOP-V9T84V9>
2023-05-15 18:25:44 +0530

    Java function

---
+++ mavenpractice/src/main/java/javafunctions/samplejen.java
@@ -0,0 +1,13 @@
+package javafunctions;
+
+public class samplejen {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello Good morning");
+		System.out.println("I love cricket");
+
+	}
+
+}
